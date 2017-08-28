328394e741869e9104a016bca7d9ea760e852cec
Ghaith <limgha.e@gmail.com>
2017-08-28 11:13:05 +0200

    Update github jobs api.py

--- github jobs api.py
+++ github jobs api.py
@@ -6,4 +6,4 @@ link="http://jobs.github.com/positions.json?description="+keyword
 r=requests.get(link)
 joblist =json.loads(r.text)
 for job in joblist:
-print(joblist[job]['title'])
+    print(joblist[job]['title'])
