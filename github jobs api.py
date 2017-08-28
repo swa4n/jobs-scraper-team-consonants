1b0fcdf0b0cb68d894d6fa5b19161b93b9ce075b
swa4n <31397948+swa4n@users.noreply.github.com>
2017-08-28 11:07:38 +0200

    Original file

---
+++ github jobs api.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+input(keyword)
+link="http://jobs.github.com/positions.json?description="+keyword
+r=requests.get(link)
+joblist =json.loads(r.text)
+for job in joblist:
+print(joblist[job]['title'])
