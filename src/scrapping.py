4c07033a0627bc4682984e8ed42546e769ffb468
Ghaith <limgha.e@gmail.com>
2017-08-29 13:16:13 +0200

    Add files via upload

--- src/scrapping.py
+++ src/scrapping.py
@@ -1,18 +1,27 @@
 import requests
 import json
+import re
+import urllib as ul
 from source import *
 def main(db):
-	print("Enter the desired tag between quotes and replacing spaces with +")
-	keyword= input()
+	while True:
+		keyword=raw_input("Enter the desired keyword\n")
+		if keyword.isdigit():
+			print("Error! entry is a number")
+		break
+	keyword=re.sub('"', '', keyword)
+	keyword=ul.quote_plus(keyword.encode('utf-8'))
+	print(keyword)
 	i=1
 	for link in links:
-		link.replace("{{keyword}}",keyword)
-		print("Requesting link "+str(i)+" ...")
+		link=link.replace("{{keyword}}",keyword)
+		print(link)
+		print("Requesting link %d ..." %i)
 		r=requests.get(link)
 		print("Request complete, converting to array of JSON objects...")
 		joblist =json.loads(r.text)
-		print("Conversion complete! starting database insertion...")
+		print("Conversion complete! Number of job offers found : %d" %len(joblist))
 		for job in joblist:
 			result=db.offers.insert_one(job)
-		print("Results inserted in database for link number %s", i)
+		print("Results inserted in database for link # %d" %i)
 		i=i+1
