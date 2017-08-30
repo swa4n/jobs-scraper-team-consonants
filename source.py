dc6a3717ba9919b61b5aca16e78ebf13e41ed958
swa4n <31397948+swa4n@users.noreply.github.com>
2017-08-30 10:29:47 +0200

    Delete source.py

--- source.py
+++
@@ -1,4 +0,0 @@
-#The source file provides links corrsponding to the APIs in websites
-#The links are put in a list called links
-
-links= ["http://jobs.github.com/positions.json?description={{keyword}}", "https://remoteok.io/remote-jobs.json?tags={{keyword}}"]
